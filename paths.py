0d2a0400b6590b889045463513e73b9f8db29e09
Darkrider123 <cosmin.ariton@gmail.com>
2023-01-21 20:03:23 +0200

    changed paths to shorter versions

--- paths.py
+++ paths.py
@@ -2,11 +2,11 @@ import os
 
 DATASET_PATH = "dataset"
 
-TRAIN_SAMPLES_DIR = os.path.join(DATASET_PATH, "train_images")
+TRAIN_SAMPLES_DIR = os.path.join(DATASET_PATH, "train")
 
-VALIDATION_SAMPLES_DIR = os.path.join(DATASET_PATH, "val_images")
+VALIDATION_SAMPLES_DIR = os.path.join(DATASET_PATH, "val")
 
-TEST_SAMPLES_DIR = os.path.join(DATASET_PATH, "test_images")
+TEST_SAMPLES_DIR = os.path.join(DATASET_PATH, "test")
 
 OUTPUT_DIR = "output"
 

8a0919a74d97bd00cf29c969cf00fb9cb03b6479
Darkrider123 <cosmin.ariton@gmail.com>
2023-02-01 20:04:23 +0200

    changed model and automatizated some things

--- model_generator.py
+++ model_generator.py
@@ -1,64 +1,117 @@
 import tensorflow as tf
-from keras.layers import Conv2D, Conv2DTranspose
+from keras.layers import Conv2D, Conv2DTranspose, BatchNormalization, Dropout, ReLU, Concatenate, Activation
 
 from custom_metrics import CUSTOM_METRICS
 from util import INPUT_SIZE_NUMPY, GROUND_TRUTH_SIZE_NUMPY
 
 def make_model():
+
     input = tf.keras.Input(shape = INPUT_SIZE_NUMPY)
 
     layer = input
 
 
-    d1 = Conv2D(64, (3, 3), activation="relu")(layer)
-    d2 = Conv2D(64, (3, 3), activation="relu")(d1)
+    layer = Conv2D(64, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.2)(layer)
+    layer = ReLU()(layer)
+
+    layer = Conv2D(64, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.2)(layer)
+    layer = ReLU()(layer)
+    d1 = layer
+
+
+
+    layer = Conv2D(64, 3, strides=2)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.2)(layer)
+    layer = ReLU()(layer)
     
 
-    d3 = Conv2D(64, (3, 3), strides=2, activation="relu")(d2)
-    d4 = Conv2D(64, (3, 3), activation="relu")(d3)
 
-    d5 = Conv2D(128, (3, 3), strides=2, activation="relu")(d4)
+    layer = Conv2D(128, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.3)(layer)
+    layer = ReLU()(layer)
+
+    layer = Conv2D(128, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.3)(layer)
+    layer = ReLU()(layer)
+    d2 = layer
+
+
+    layer = Conv2D(128, 3, strides=2)(layer)
+    layer = BatchNormalization()(layer)
+    layer =  Dropout(0.3)(layer)
+    layer = ReLU()(layer)
     
-    d6 = Conv2D(256, (3, 3), padding="same",activation="relu")(d5)
+    layer = Conv2D(256, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.4)(layer)
+    layer = ReLU()(layer)
+
+    layer = Conv2D(256, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.4)(layer)
+    layer = ReLU()(layer)
 
 
-    s1 = Conv2D(350, (3, 3), padding="same",activation="relu")(d6)
-    layer = Conv2D(350, (3, 3), padding="same",activation="relu")(s1)
-    layer = layer + s1
-
-    layer = Conv2DTranspose(256, (3, 3), activation="relu")(layer)
-    layer = Conv2D(256, (3, 3), activation="relu")(layer)
-    layer = layer + d6
-
-    layer = Conv2DTranspose(128, (3, 3), activation="relu")(layer)
-    layer = Conv2D(128, (3, 3), activation="relu")(layer)
-    layer = layer + d5
-
-    layer = Conv2DTranspose(128, (3, 3), strides=2, activation="relu")(layer)
-    layer = Conv2DTranspose(64, (2, 2), activation="relu")(layer)
-    layer = layer + d4
     
-    layer = Conv2DTranspose(64, (3, 3), activation="relu")(layer)
-    layer = layer + d3
 
-    layer = Conv2DTranspose(64, (4, 4), strides=2, activation="relu")(layer)
-    layer = layer + d2
+    layer = Conv2DTranspose(128, 4, strides=3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.3)(layer)
+    layer = ReLU()(layer)
+    layer = Concatenate()([layer, d2])
 
-    layer = Conv2DTranspose(64, (3, 3), activation="relu")(layer)
-    layer = layer + d1
+    layer = Conv2D(128, 4)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.3)(layer)
+    layer = ReLU()(layer)
     
-    layer = Conv2DTranspose(3, (3, 3), activation="tanh")(layer)
+    layer = Conv2D(128, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.3)(layer)
+    layer = ReLU()(layer)
 
-    layer = layer + input
 
-    layer = tf.maximum(layer, 0)
+    layer = Conv2DTranspose(64, 3, strides=3)(layer)
+    layer = BatchNormalization()(layer)
+    layer =  Dropout(0.2)(layer)
+    layer = ReLU()(layer)
+    layer = Concatenate()([layer, d1])
 
-    layer = tf.minimum(layer, 1)
+    layer = Conv2D(128, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.2)(layer)
+    layer = ReLU()(layer)
+
+    layer = Conv2D(128, 3)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.2)(layer)
+    layer = ReLU()(layer)
+
+    
+    layer = Conv2DTranspose(64, 3, dilation_rate=4)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dropout(0.2)(layer)
+    layer= ReLU()(layer)
+    layer = Concatenate()([layer, input])
+
+
+    layer = Conv2D(GROUND_TRUTH_SIZE_NUMPY[2], 1)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Activation(activation="tanh")(layer)
+
+    layer = (layer + 1) / 2
 
     layer = layer * 255
 
 
-                
+
     model = tf.keras.Model(inputs = input, outputs = layer)
     model.compile(loss="mean_squared_error", optimizer="adam", metrics=CUSTOM_METRICS)
 
--- preprocessing.py
+++ preprocessing.py
@@ -5,13 +5,13 @@ from config import NOISE_PERCENTAGE
 def preprocess_image_training(image, label):
     image = tf.cast(image, tf.float32)
 
-    image = image / 255
+    image = image / 255 * 2 - 1
 
     noise = tf.random.normal(shape=image.shape, mean=0, stddev=0.1, dtype=tf.float32) * tf.floor(tf.random.uniform(shape=image.shape) * (1 + NOISE_PERCENTAGE))
 
     image = image + noise
 
-    image = tf.maximum(image, 0)
+    image = tf.maximum(image, -1)
     image = tf.minimum(image, 1)
 
     return image, label
@@ -19,6 +19,6 @@ def preprocess_image_training(image, label):
 def preprocess_image_predicting(image, label):
     image = tf.cast(image, tf.float32)
 
-    image = image / 255
+    image = image / 255 * 2 - 1
 
     return image, label
